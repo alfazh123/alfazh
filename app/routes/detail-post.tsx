d9eb773f58c9a1290e6b84268e79f2d794f29007
alfazh123 <alfazh291@gmail.com>
2026-05-21 14:29:04 +0700

    update detail posts banner and text

--- app/routes/detail-post.tsx
+++ app/routes/detail-post.tsx
@@ -25,7 +25,7 @@ export default function DetailPost() {
 	}, [slug]);
 
 	return (
-		<div>
+		<div className="pt-20">
 			{frontMatter?.bannerImgUrl && (
 				<BlogBanner imgUrl={frontMatter?.bannerImgUrl} />
 			)}
@@ -36,7 +36,7 @@ export default function DetailPost() {
 					"prose-p:mx-auto prose-p:max-w-2xl prose-p:w-full prose-p:text-lg prose-p:mb-0 prose-img:mb-0 prose-img:rounded-lg prose-img:border prose-img:mx-auto",
 					"prose-code:before:content-none prose-code:after:content-none",
 					"prose-ul:mx-auto prose-ul:max-w-2xl prose-ul:w-full prose-ol:mx-auto prose-ol:max-w-2xl prose-ol:w-full prose-li:mb-0",
-					"dark:prose-p:text-white dark:prose-headings:text-white dark:prose-li:text-white dark:prose-code:text-white dark:prose-a:text-white",
+					"dark:prose-p:text-slate-200 dark:prose-headings:text-white dark:prose-li:text-white dark:prose-code:text-white dark:prose-a:text-white dark:prose-strong:text-white",
 					"relative mt-20 flex flex-col justify-center min-h-screen max-w-4xl w-full mx-auto px-8 mb-20",
 				)}>
 				<div className="absolute md:flex hidden top-0 right-0 w-fit px-2 h-10 bg-amber-200 rotate-12 z-10 items-center justify-center">
