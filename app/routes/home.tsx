397e21718ac72aa8da878b5f1e2b2c9c5f5ef2e9
alfazh123 <alfazh291@gmail.com>
2026-06-03 17:11:19 +0700

    fix metadata

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,13 +4,6 @@ import PostList from "~/components/post-list";
 import ProjectList from "~/components/project-list";
 import Experience from "~/components/experience";
 
-export function meta({}: Route.MetaArgs) {
-	return [
-		{ title: "Alfazh" },
-		{ name: "description", content: "Alfazh portfolio" },
-	];
-}
-
 export default function Home() {
 	return (
 		<div className="home px-4 overflow-clip">
