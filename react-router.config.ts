a559692fa572c13d130858d120dc27bae37ce4f3
alfazh123 <alfazh291@gmail.com>
2026-06-06 16:32:41 +0700

    it didnt work

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,22 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
-	ssr: true,
-	prerender: [
-		"/",
-		"/about",
-		"/project",
-		"/blog",
-		"/piano",
-		// Dynamic blog posts
-		"/blog/build-blog-with-nextjs",
-		"/blog/built-wasm-package-with-rust",
-		"/blog/i-like-hotkeys",
-		"/blog/make-reusable-component-react",
-		"/blog/what-i-like-in-my-portfolio",
-		// Dynamic projects
-		"/project/ace24",
-		"/project/image-editor",
-		"/project/pathxplorer",
-	],
+	ssr: false,
+	prerender: true,
 } satisfies Config;
