7200d91096e885453b0748953b32679aa0bbfea2
alfazh123 <alfazh291@gmail.com>
2026-06-07 15:24:48 +0700

    set static url in prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,21 @@ export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: true,
-	prerender: true,
+	prerender: [
+		"/",
+		"/about",
+		"/project",
+		"/blog",
+		"/piano",
+		// Dynamic blog posts
+		"/blog/build-blog-with-nextjs",
+		"/blog/built-wasm-package-with-rust",
+		"/blog/i-like-hotkeys",
+		"/blog/make-reusable-component-react",
+		"/blog/what-i-like-in-my-portfolio",
+		// Dynamic projects
+		"/project/ace24",
+		"/project/image-editor",
+		"/project/pathxplorer",
+	],
 } satisfies Config;
